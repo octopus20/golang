3cd570e0a87c9342523fd44c9c283093a95210b6
octopus20 <abeermso11@gmail.com>
2022-10-08 14:08:26 +0300

    Abeer: add input validation

--- simple-math/main.go
+++ simple-math/main.go
@@ -12,13 +12,13 @@ func main() {
 	fmt.Print("Enter first number : ")
 	// Scan and validate user input
 	if _, err := fmt.Scan(&first); err != nil {
-		log.Print("  Scan for inputs failed, due to ", err)
+		log.Print("  Scan for first input failed, due to ", err)
 		return
 	}
 	fmt.Print("Enter second number : ")
 	// Scan and validate user input
 	if _, err := fmt.Scan(&second); err != nil {
-		log.Print("  Scan for inputs failed, due to ", err)
+		log.Print("  Scan for second input failed, due to ", err)
 		return
 	}
 	equation = first * second

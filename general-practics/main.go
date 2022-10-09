69f4f0ed8765a51454638ba47e06b8c29c574fc7
octopus20 <abeermso11@gmail.com>
2022-10-09 18:31:30 +0300

    Abeer: calculate total and avr of array

---
+++ general-practics/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	myarray := []int{1, 2, 3, 4, 5}
+	sum := 0
+
+	for i := 0; i < (len(myarray)); i++ {
+		sum += (myarray[i])
+	}
+	avg := (float64(sum)) / (float64(len(myarray)))
+
+	fmt.Println("Total = ", sum, "\nAverage = ", avg)
+}
